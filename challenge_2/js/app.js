022a696df4f394072c90cb267fcd722aecd3f6af
Kevin Tran <kevin2005tran@gmail.com>
2016-11-28 22:05:12 -0800

    finished half of challenge 2, will finish the rest tomorrow

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,15 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Kevin Tran",
+  this.age = 28,
+  this.city = "Alameda",
+  this.state = "California"
+  this.shout = function(word){
+    return word.toUpperCase() + "!";
+  }
+
+}
